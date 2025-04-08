e6942e80228dc9d931fbc9e4a322ef92a73d165f
Amal Babu <amalbabu1200@gmail.com>
2025-04-08 11:31:20 +0530

    migrations, docker build

--- fastapi_admin/cli.py
+++ fastapi_admin/cli.py
@@ -1,17 +1,24 @@
 import typer
-from fastapi_admin.commands import project, app, server, container
+from fastapi_admin.commands import project, app, server, container, migrations, docker
 
-cli_app = typer.Typer(help="FastAPI Admin CLI tool for managing FastAPI applications")
+cli_app = typer.Typer(
+    help="FastAPI Admin CLI tool for managing FastAPI applications")
 
 # Register commands
 cli_app.add_typer(project.app, name="startproject")
 cli_app.add_typer(app.app, name="startapp")
 cli_app.add_typer(server.app, name="runserver")
 cli_app.add_typer(container.app, name="shell")
+# Access as: fastapi-admin db migrate
+cli_app.add_typer(migrations.app, name="db")
+# Access as: fastapi-admin docker build
+cli_app.add_typer(docker.app, name="docker")
+
 
 def cli():
     """Entry point for the CLI tool"""
     cli_app()
+
 
 if __name__ == "__main__":
     cli()
